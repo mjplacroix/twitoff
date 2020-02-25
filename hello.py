0f7c52ec49f25c6cf76115e63bea281bb72340c5
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-02-25 10:55:01 -0500

    connection User to Tweets in one-to-many relationship

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 # make the application
 app = Flask(__name__)
@@ -8,6 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 
 #def a function
-def hello:
+def hello():
     return "Hello World!!"
+
+# make second route
+@app.route("/about")
+
+# func for about
+def preds():
+    return render_template('about.html')
 
