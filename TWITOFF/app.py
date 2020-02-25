a26850c7d170aa5e8459fe8cb5487c4aa352f608
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-02-25 15:00:42 -0500

    building DB out with Twitter and Basilica embeddings

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -2,7 +2,7 @@
 
 from decouple import config
 from flask import Flask, render_template, request
-from .models import DB
+from .models import DB, User
 
 # make our app factory
 
@@ -11,11 +11,23 @@ def create_app():
 
     #add config for database
     app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.sqlite3'
+
+    #stop error message about SQLite overload
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     # have DB know about the app
     DB.init_app(app)
 
 
     @app.route('/')
     def root():
-        return render_template('base.html')
+        users = User.query.all()
+        return render_template('base.html', title= 'Homie', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title = 'Reset', user=[])
+
     return app
