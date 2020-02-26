515e08fa5a8ff445dea4458770e9784885e09301
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-02-26 14:43:16 -0500

    some more stuff <-- terrible message I know

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -3,17 +3,27 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .predict import predict_user
 from .twitter import add_or_update_user
+# import pdb
 
 # make our app factory
-
 def create_app():
     app = Flask(__name__)
 
     #add config for database
     app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.sqlite3'
+    #stop error message about SQLite overload
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # have DB know about the app
+    DB.init_app(app)
 
-    @app.route('/user', methods=['POST', 'GET'])
+    @app.route('/')
+    def root():
+        users = User.query.all()
+        return render_template('base.html', title= 'Homie', users=users)
+
+    @app.route('/user', methods=['POST'])
     @app.route('/user/<name>', methods=['GET'])
     def user(name=None, message=''):
         name = name or request.values['user_name']
@@ -27,20 +37,19 @@ def create_app():
             tweets = []
         return render_template('user.html', title=name, tweets=tweets, 
         message=message)
-
-    #stop error message about SQLite overload
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # have DB know about the app
-    DB.init_app(app)
-
-
-    @app.route('/')
-    def root():
-        users = User.query.all()
-        return render_template('base.html', title= 'Homie', users=users)
-
     
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        # pdb.set_trace()
+        user1, user2 = sorted([request.values['user1'], request.values['user2']])
+        if user1 == user2:
+            message = "Can't compare a user  to themselves..."
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2, 
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
 
     @app.route('/reset')
     def reset():
