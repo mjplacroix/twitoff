0074794fa57da8d9ae3c328b04492181b6cf0647
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-08-11 18:55:28 -0400

    edit DB root

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,5 +1,7 @@
 """Code for our app"""
 
+from os import getenv
+
 # from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
@@ -12,7 +14,8 @@ def create_app():
     app = Flask(__name__)
 
     #add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.sqlite3'
+    # app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
     #stop error message about SQLite overload
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # have DB know about the app
